86c9957a902f0f3500e961fbc0760a2815c6a5f6
Ivan Kalinchuk <ivankalinchuk@Ivans-MacBook-Pro.local>
2022-05-01 15:39:31 +0300

    updated main with debug removal

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from typing import Optional
 
-import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -14,9 +13,3 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
-
-if __name__ == "__main__":
-    uvicorn.run(app, port=8000) #host="0.0.0.0",
-
-
-
