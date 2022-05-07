dd6d281e352be028af90ed1d41eb29b6b6cf2c20
Ivan Kalinchuk <ivankalinchuk@Ivans-MacBook-Pro.local>
2022-05-07 03:57:40 +0300

    added analyze endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from nltk.tokenize import word_tokenize
 
 app = FastAPI()
 
@@ -8,6 +9,13 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.post("/analyze")
+def analyze(text: str):
+    text_size = len(text)
+    num_tokens = len(word_tokenize(text))
+    return {"textSize": text_size,
+            "numWords": num_tokens}
 
 
 @app.get("/items/{item_id}")
