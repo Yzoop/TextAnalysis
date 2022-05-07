53889aee38baeda3356f96d0669bed81b4889301
Ivan Kalinchuk <ivankalinchuk@Ivans-MacBook-Pro.local>
2022-05-07 03:37:48 +0300

    added gridsearch for dataset size

--- features/train_text_classifier.py
+++ features/train_text_classifier.py
@@ -2,6 +2,8 @@ import json  # allows reading json data into python dictionary
 
 import numpy as np
 import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
 
 from sklearn.model_selection import train_test_split  # splits data into train and validational sets
 from sklearn.feature_extraction.text import TfidfVectorizer  # converts texts to vectors
@@ -16,6 +18,7 @@ def texts_labels_from_data(path, dataset_size=1):
         # read tagot labeled data
         data = json.load(f)
     # TODO: change dataset size from 0 (0 examples) to 1 (all examples). 0.6 - 60% of examples
+    np.random.seed(17)
     np.random.shuffle(data)
     data = data[:int(len(data) * dataset_size)]
     # split (text, label) pairs into 2 seperate lists for train_test_split function
@@ -46,11 +49,16 @@ def train_classifier(train_x, train_y):
     logreg_model = LogisticRegression(random_state=17, penalty="none")
     multiclass_classifier = MultiOutputClassifier(
         logreg_model)  # multiclass classifier https://scikit-learn.org/stable/modules/generated/sklearn.multiclass.OneVsRestClassifier.html
+    # for x, y in zip(train_x, train_y):
+    #     try:
+    #         multiclass_classifier.fit(train_x, train_y)
+    #     except Exception as e:
+    #         print(e, "exception on", y)
     multiclass_classifier.fit(train_x, train_y)
     return multiclass_classifier
 
 
-def print_metrics(model, label_binarizer, train_x, test_x, train_y, test_y):
+def calculate_metrics(model, label_binarizer, train_x, test_x, train_y, test_y):
     print("Train accuracy:", accuracy_score(train_y, model.predict(train_x)))
     print("Test accuracy:", accuracy_score(test_y, model.predict(test_x)))
     prediction_test_y = model.predict(test_x)
@@ -59,15 +67,40 @@ def print_metrics(model, label_binarizer, train_x, test_x, train_y, test_y):
     index_dict = {idx: class_name for idx, class_name in zip(report_df.index, label_binarizer.classes_)}
     report_df.rename(index=index_dict,
                      inplace=True)
-    print(report_df)
+    return report_df
+
+
+def plot_validation_curve(texts, labels, dataset_sizes):
+    micro_avgs = []
+    for size in dataset_sizes:
+        texts_samples, labels_samples = texts[:int(len(texts) * size)], labels[:int(len(labels) * size)]
+        text_vectors, label_vectors, label_binarizer = convert_data_to_vectors(texts_samples,
+                                                                               labels_samples,
+                                                                               return_binarizer=True)
+        train_x, test_x, train_y, test_y = train_test_split(text_vectors,
+                                                            label_vectors,
+                                                            test_size=0.2,
+                                                            random_state=17)
+        multiclass_classifier = train_classifier(train_x, train_y)
+        metrics = calculate_metrics(multiclass_classifier, label_binarizer, train_x, test_x, train_y, test_y)
+        micro_avg_f1 = metrics.loc[pd.Index(["micro avg"]), pd.Index(["f1-score"])].values[0][0]
+        micro_avgs.append(micro_avg_f1)
+    ax = sns.lineplot(x=dataset_sizes, y=micro_avgs)
+    ax.set(xlabel='dataset size', ylabel='avg f1')
+
+    plt.show()
 
 
 if __name__ == "__main__":
     texts, labels = texts_labels_from_data("../data/text_classification_dataset_2022-05-07.json")
-    text_vectors, label_vectors, label_binarizer = convert_data_to_vectors(texts, labels, return_binarizer=True)
+    text_vectors, label_vectors, label_binarizer = convert_data_to_vectors(texts,
+                                                                           labels,
+                                                                           return_binarizer=True)
     train_x, test_x, train_y, test_y = train_test_split(text_vectors,
                                                         label_vectors,
                                                         test_size=0.2,
                                                         random_state=17)
     multiclass_classifier = train_classifier(train_x, train_y)
-    print_metrics(multiclass_classifier, label_binarizer, train_x, test_x, train_y, test_y)
+    metrics = calculate_metrics(multiclass_classifier, label_binarizer, train_x, test_x, train_y, test_y)
+    print(metrics)
+    plot_validation_curve(texts, labels, dataset_sizes=[0.2, 0.25, 0.5, 0.6, 0.8, 1.0])
